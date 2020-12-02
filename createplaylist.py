7cba01b12b60de1c1392ab447d1f61edd5467151
Michiel DeKoninck <michieldekoninck2@gmail.com>
2020-12-02 18:31:08 +0100

    os

--- createplaylist.py
+++ createplaylist.py
@@ -6,14 +6,14 @@ from sklearn.neighbors import KNeighborsRegressor
 from dotenv import load_dotenv
 import os
 load_dotenv()
-import boto
-from boto.s3.connection import S3Connection
-client_id = S3Connection(os.environ['CLIENT_ID'])
-client_secret = S3Connection(os.environ['CLIENT_SECRET'])
-redirect = S3Connection(os.environ['SPOTIPY_REDIRECT_URI'])
-#client_id = os.environ.get('CLIENT_ID')
-#client_secret = os.environ.get('CLIENT_SECRET')
-#user_id = os.environ.get('user_id')
+#import boto3
+#from boto.s3.connection import S3Connection
+#client_id = S3Connection(os.environ['CLIENT_ID'])
+#client_secret = S3Connection(os.environ['CLIENT_SECRET'])
+#redirect = S3Connection(os.environ['SPOTIPY_REDIRECT_URI'])
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+user_id = os.environ.get('user_id')
 
 import spotipy
 #Authentication with Spotipy package
