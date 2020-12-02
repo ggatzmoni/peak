32dfe7f160524cf5531b536e6dd981754e6e4616
Michiel DeKoninck <michieldekoninck2@gmail.com>
2020-12-02 12:47:11 +0100

    production

---
+++ app.py
@@ -0,0 +1,118 @@
+'''imports'''
+from flask import Flask, redirect, url_for, render_template, request, session
+from function import genre, decade, track, length, popularity
+from createplaylist import main
+from werkzeug.datastructures import ImmutableMultiDict
+from dotenv import load_dotenv
+import os
+load_dotenv()
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+user_id = os.environ.get('user_id')
+authorization_token = os.environ.get('authorization_token')
+from createplaylist import filter_data, get_seed, fit_model, train_model, filter_sort
+
+import spotipy
+#Authentication with Spotipy package
+from spotipy.oauth2 import SpotifyOAuth
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri="https://example.com", #replace with our website url
+                                               scope="playlist-modify-public"))
+
+#Import classes from other files
+from spotifyclient import SpotifyClient
+from track import Track
+
+'''instance flask web application'''
+app = Flask(__name__)
+
+#config parameters database
+
+
+
+'''create DB'''
+
+
+'''define pages on app'''
+# access to page via function decorater - start page
+#@app.route("/")
+# home-app with inline html
+#def home():
+    #return render_template("index.html")
+
+# acces to page via parameter
+#@app.route("/<name>")
+#speficy different pages via f string
+#def user(name):
+    #return render_template("v1")
+
+#redirect to home when on admin page
+
+#create player page
+#@app.route("/player", methods=['POST','GET'])
+#def player():
+    #if request.main == 'POST':
+        #msg = request.form.get('msg')
+        #print(msg)
+    #return render_template("player.html", main=main)
+
+#@app.route("/v1")
+#def v1():
+    #return render_template("v1.html")
+
+@app.route("/algo_running", methods=["POST","GET"])
+def algo():
+    return render_template("algo_running.html")
+
+@app.route("/playlist", methods=["POST","GET"])
+def playlist():
+    return render_template("playlist.html")
+
+@app.route("/page_player", methods=["POST","GET"])
+def page_player():
+    return render_template("page_player.html", length=length, genre=genre, decade=decade, popularity=popularity)
+
+@app.route("/error", methods=["POST","GET"])
+def error():
+    return render_template("experiment.html")
+
+@app.route("/algo_running", methods=["POST","GET"])
+def algo_running():
+    return render_template("algo_running.html", length=length, genre=genre, decade=decade, popularity=popularity)
+
+@app.route("/", methods=["POST","GET"])
+def center():
+    genres = genre()
+    decades = decade()
+    tracks = track()
+    lengths = length()
+    popularities = popularity()
+    return render_template("center.html", genres=genres, decades=decades, tracks = tracks, lengths = lengths, popularities = popularities)
+
+
+@app.route("/algo_input", methods=["POST","GET"])
+def algo_input():
+    if request.method == "POST":
+        req = request.form
+        genre, decade, length, popularity = req.values()
+        #specify function to input front-end and return html page
+        if filter_data(genre, decade, popularity, length) is False:
+            return redirect("/error")
+        result = filter_sort(genre, decade, popularity, length)
+        #session['genre'] = 'rock'
+
+        return render_template('algo_running.html', length=length, genre=genre, decade=decade, popularity=popularity)  #redirect('/algo_running'
+
+    return render_template("center.html")
+
+@app.route("/playlist_input", methods=["POST","GET"])
+def playlist_input():
+    if request.method == "POST":
+        playlist_name = request.form['playlist_name']
+        return render_template('page_player.html', playlist_name=playlist_name)
+    return redirect("/error")
+
+'''run app'''
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ createplaylist.py
@@ -0,0 +1,172 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
+#Importing stuffs for authentication with Spotify API
+from dotenv import load_dotenv
+import os
+load_dotenv()
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+user_id = os.environ.get('user_id')
+authorization_token = os.environ.get('authorization_token')
+
+import spotipy
+#Authentication with Spotipy package
+from spotipy.oauth2 import SpotifyOAuth
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri="https://example.com", #replace with our website url
+                                               scope="playlist-modify-public"))
+
+#Import classes from other files
+from spotifyclient import SpotifyClient
+from track import Track
+
+
+'''updates michiel'''
+from IPython.display import HTML
+#Dataframe final
+
+def get_data():
+    df_kaggle = pd.read_csv('full_dataset_new.csv')
+    df_kaggle['year'] = df_kaggle['year'].astype(str)
+    return df_kaggle
+
+#filter data
+def filter_data(genre, decade, popularity, length):
+    df_kaggle = get_data()
+    filtered_results = df_kaggle[(df_kaggle['genres'].str.contains(str.lower(genre))) & (df_kaggle['decades'] == decade) & (df_kaggle['popularity_binned'] == popularity)]
+    if sum(filtered_results['duration_min'].cumsum()) < float(length):
+        return False
+    return filtered_results
+
+def get_seed(genre, decade, popularity, length):
+    filtered_results = filter_data(genre, decade, popularity, length)
+    seed = filtered_results.sample(1)
+    tempo = seed['scaled_tempo'].iat[0]
+    loudness = seed['scaled_loudness'].iat[0]
+    da = seed['danceability'].iat[0]
+    energy = seed['energy'].iat[0]
+    return seed, tempo, loudness, da, energy
+
+def fit_model(genre, decade, popularity, length):
+    filtered_results = filter_data(genre, decade, popularity, length)
+    features_names = ['scaled_tempo', 'scaled_loudness', 'danceability', 'energy'] # 'scaled_year', 'popularity_binned'
+    X = filtered_results[features_names]
+    y = filtered_results['track_id']
+    model = KNeighborsRegressor(algorithm='kd_tree', n_jobs=-1).fit(X, y)
+    return model
+
+def train_model(genre, decade, popularity, length):
+    # get trained model output for k: distances & indices
+    model = fit_model(genre,decade,popularity, length)
+    seed, tempo, loudness, da, energy = get_seed(genre,decade,popularity, length)
+    filtered_results = filter_data(genre, decade, popularity, length)
+    knn_out, k = [], (len(filtered_results) if len(filtered_results)<100 else 100)
+    knn_out = model.kneighbors([[tempo,loudness,da,energy]], n_neighbors=k)
+    ind = knn_out[1][0].tolist() # get indices
+    recs = filtered_results.iloc[ind] # recommendations df
+    return recs
+
+def filter_sort(genre, decade, popularity, length):
+    filtered_results2 = train_model(genre, decade, popularity, length)
+    filtered_duration = filtered_results2[filtered_results2['duration_min'].cumsum() <= int(length)]
+    recommended_playlist = filtered_duration.reset_index(drop=True)
+    recommended_tracks = recommended_playlist[['track_name','track_id','artists']]
+    return recommended_tracks
+
+#def get_playlist(genre, decade, popularity, length, playlist_name):
+    #sorted_playlist = filter_sort(genre, decade, popularity, length)
+    #playlist = spotify_client.create_playlist(playlist_name)
+    #playlist_id = playlist.playlist_id
+    ## populate playlist with recommended tracks
+    #tracks_id = sorted_playlist['track_id'].tolist()
+    ##sp.playlist_add_items(playlist_id, tracks_id, position=None)
+    #track_uris = [create_spotify_uri(track) for track in tracks_id]
+    #response = requests.post(
+        #url=f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks",
+        #data = json.dumps(track_uris),
+        #headers={
+            #"Content-Type": "application/json",
+            #"Authorization": f"Bearer {authorization_token}"
+        #}
+    #)
+    #response = response.json()
+
+
+
+
+#Will be replaced by preprocessing pipeline
+'''def formatting(df):
+    df['genres'] = df['genres'].astype(str)
+    df['duration_min'] = (df['duration_ms']/60000).astype(int)
+    df['decades'] = pd.cut(x=df['year'], bins=[1920, 1930, 1940, 1950,1960,1970,1980,1990,2000,2010,2020])
+    df['year'] = df['year'].astype(str)
+    return df'''
+
+#Display user input as a list of choices
+def get_choice(df, column):
+
+    #Gets user choice
+    nums = [val for val in range(len(df[column].unique()))]
+    choices = list(zip(nums, df[column].unique()))
+    print("Select '%s'\n" % column)
+    for v in choices:
+        print("%s.  %s" % (v))
+    user_input = input("Answer: ")
+    user_answer = [val[1] for val in choices if val[0]==int(user_input)][0]
+    print("'%s' = %s\n" % (column, user_answer))
+    return user_answer
+
+#def getparam(genre, decade, length, popularity):
+    #playlist = [genre, decade, length, popularity]
+    #return playlist
+
+# get filtered data for html
+
+def main():
+    spotify_client = SpotifyClient(authorization_token,user_id)
+
+    #Asking users for their preferences
+    query_genre=input("Which genre?\n>")
+    query_pop = input("Popularity? 0 and 100, with 100 being the most popular\n> ")
+    query_decade = get_choice(df=get_data(), column="decades")
+    query_duration = input("Duration of the playlist?\n> ")
+    print(f" You selected {query_genre} tracks with a popularity of {query_pop}% from the {query_decade} decade for a total duration of {query_duration} minutes")
+
+    #Converting to the right type
+    query_pop = int(query_pop)
+    query_duration = int(query_duration)
+    query_decade = str(query_decade)
+    query_genre = str(query_genre)
+
+    #filtering the dataset accordingly
+    df_kaggle = get_data()
+    filtered_genre = df_kaggle[df_kaggle['genres'].str.contains(query_genre)]
+    filtered_results = filtered_genre[(filtered_genre['year'].str.contains(query_decade[1:3])) & (filtered_genre['popularity'] == query_pop)]
+
+    # Select 1 random track seed from the filtered_results
+    # Insert here the recommendation algorythm from Luam
+
+    # (filtered_results will be replaced by the name of Luam's output dataframe)
+    filtered_duration = filtered_results[filtered_results['duration_min'].cumsum() <= query_duration]
+    recommended_playlist = filtered_duration.reset_index(drop=True)
+    recommended_tracks = recommended_playlist[['track_name','track_id','artists']]
+    print(recommended_tracks)
+
+
+    # get playlist name from user and create playlist
+    playlist_name = input("\nWhat's the playlist name? ")
+    playlist_name = str(playlist_name)
+    playlist = spotify_client.create_playlist(playlist_name)
+    playlist_id =playlist.id
+    print(f"\nPlaylist '{playlist.name}' was created successfully.")
+
+    # populate playlist with recommended tracks
+    tracks_id = recommended_playlist['track_id'].tolist()
+    sp.playlist_add_items(playlist_id, tracks_id, position=None)
+    print(f"\nRecommended tracks successfully uploaded to playlist '{playlist_name}'.")
+
+if __name__ == "__main__":
+    main()
